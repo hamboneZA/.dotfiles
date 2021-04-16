567512541ef51038cac965cb6f2784759023c8d8
hamboneZA <31657055+hamboneZA@users.noreply.github.com>
2021-04-16 04:15:58 +0200

    Update make-nice.sh

--- make-nice.sh
+++ make-nice.sh
@@ -27,11 +27,18 @@ fi
 #--------------------------------------------------------------------------------
 echo "Install basic Homebrew packages..."
 brew install python
-brew install git bash-completion
-brew install libsndfile libsoundio portaudio sox audacity spotifyd 
+brew install git bash-completion tmux
+brew install libsndfile libsoundio portaudio sox audacity spotifyd ffmpeg
 
 #--------------------------------------------------------------------------------
 # Enable Brew Services
 #--------------------------------------------------------------------------------
 
 brew services start spotifyd
+
+#--------------------------------------------------------------------------------
+# Other Stuff I Never Use
+#--------------------------------------------------------------------------------
+
+git clone https://github.com/mattstauffer/ohmyzsh-dotfiles.git && sh ohmyzsh-dotfiles/bootstrap.sh
+wget https://github.com/stashapp/stash/releases/download/latest_develop/stash-osx && chmod u+x stash-osx && tmux new-session -d -s stash '.stash-osx --port 9090'
